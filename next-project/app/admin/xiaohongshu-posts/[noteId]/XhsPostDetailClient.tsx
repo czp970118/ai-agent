b5b948651f369d4204bc9eb1a624f48b4d11b00f
czp970118 <493748703@qq.com>
2026-04-29 22:00:18 +0800

    feat:nginx

--- next-project/app/admin/xiaohongshu-posts/[noteId]/XhsPostDetailClient.tsx
+++ next-project/app/admin/xiaohongshu-posts/[noteId]/XhsPostDetailClient.tsx
@@ -18,9 +18,9 @@ function getMcpBaseUrl(): string {
   const env = process.env.NEXT_PUBLIC_MCP_SERVER_URL?.trim();
   if (env) return env.replace(/\/+$/, "");
   if (typeof window !== "undefined") {
-    return `${window.location.protocol}//${window.location.hostname}:8000`;
+    return window.location.origin;
   }
-  return "http://127.0.0.1:8000";
+  return "http://127.0.0.1";
 }
 
 export default function XhsPostDetailClient({ noteId }: { noteId: string }) {
